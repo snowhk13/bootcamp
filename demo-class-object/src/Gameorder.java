9a8a654d0b7a441e496ea1b61dd787436cda0aa4
alexanderwong <snowhk10@gmail.com>
2025-04-23 21:25:35 +0800

    .

--- demo-class-object/src/Gameorder.java
+++ demo-class-object/src/Gameorder.java
@@ -1,3 +1,72 @@
-public class Gameorder {
+import java.math.BigDecimal;
+
+public class GameOrder {
+
+  private static long counter = 0;
+
+  private Game[] games;
+  private long id;
+
+  public GameOrder(){
+    this.games = new Game[0];
+    ++counter;
+    this.id = counter;
+  }
+
+  public Game[] getGames() {
+    return games;
+  }
+
+  public void setGames(Game[] games) {
+    this.games = games;
+  }
+
+  public void addGame(Game game){
+    Game[] newGame = new Game[this.games.length+1];
+    for(int i=0;i<this.games.length;i++){
+      newGame[i] = this.games[i];
+    }
+    newGame[newGame.length-1] = game;
+    this.games = newGame;
+  }
+
+  public double amount(){
+   BigDecimal amount = BigDecimal.valueOf(0.0);
+   for(int i=0; i<this.games.length;i++){
+    amount = BigDecimal.valueOf(this.games[i].amount()).add(amount);
+   }
+   return amount.doubleValue();
+  }
+
+  public void clearOrder() {
+    this.games = new Game[0];
+}
+  
+  public static void main(String[] args) {
+    
+    GameOrder order1 = new GameOrder();
+    GameOrder order2 = new GameOrder();
+    GameOrder order3 = new GameOrder();
+
+    Game war3 = new Game("warcraft3", 500, 1);
+    Game d4 = new Game("Diablo4",450,2);
+    Game gundam = new Game("GUNDAM",300,3);
+
+    order1.addGame(war3);
+    order1.addGame(d4);
+    order1.addGame(gundam);
+
+    System.out.println(order1.amount());
+
+    System.out.println(order1.id);
+    System.out.println(order3.id);
+
+    GameOrder order4 = new GameOrder();
+
+    System.out.println(counter);
+
+
+
+  }
   
 }
--- demo-class-object/src/game.java
+++ demo-class-object/src/game.java
@@ -1,4 +1,52 @@
-public class game {
+import java.math.BigDecimal;
+
+public class Game {
   private String name;
   private double price;
+  private int quantity;
+
+  public Game() {
+
+  }
+
+  public Game(String name, double price, int quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public double amount() {
+    return BigDecimal.valueOf(this.price)
+        .multiply(BigDecimal.valueOf(this.quantity)).doubleValue();
+  }
+
+
+
+
+
+
 }
